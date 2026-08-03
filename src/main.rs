596ab414e6735d7a1f9691adfef6a831cc860d48
ROG <luanroger.dev@gmail.com>
2026-08-03 17:30:57 -0300

    feat: add support for non parallel task

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ fn main() {
     let args = Args::parse();
     let start = Instant::now();
 
-    let result = scan_folder(&args.path);
+    let result = scan_folder(&args.path, args.parallel);
 
     let execution_time = start.elapsed();
     println!("Execution time: {:?}", execution_time);
     match &result {
-        Ok(scan) => println!("Found: {} entries", scan.processed_files()),
+        Ok(scan) => println!("Scanned: {} files", scan.processed_files()),
         Err(e) => println!("Error: {}", e.0),
     }
 
--- src/sniffer.rs
+++ src/sniffer.rs
@@ -54,51 +54,66 @@ impl ScanResult {
     }
 }
 
-pub fn scan_folder(folder: &str) -> Result<ScanResult, ScanError> {
+fn filter_dir(entry: &walkdir::DirEntry) -> bool {
+    entry.path().extension().is_some_and(|extension| {
+        let ext_str = extension.to_str();
+        if let Some(ext_str) = ext_str {
+            FILE_EXTENSION.contains(&ext_str)
+        } else {
+            false
+        }
+    })
+}
+
+fn for_each_file(
+    entry: walkdir::DirEntry,
+    result: &DashMap<String, EnvMetadata>,
+    processed_files: &AtomicUsize,
+) {
+    let content = std::fs::read_to_string(entry.path());
+    if let Err(err) = content {
+        eprintln!("Error reading file: {}", err);
+        return;
+    }
+
+    processed_files.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+    let content = content.unwrap();
+    for cap in ENV_VAR_TS_DEFAULT_PATTERN.captures_iter(&content) {
+        let var_name = String::from(&cap[1]);
+        let current_var_ammount = result.get_mut(&var_name);
+        let place = entry.path().to_string_lossy().into_owned();
+
+        match current_var_ammount {
+            Some(mut value) => {
+                value.register_place(place);
+            }
+            None => {
+                let mut new_env = EnvMetadata::new(var_name.clone());
+                new_env.register_place(place);
+                result.insert(var_name, new_env);
+            }
+        }
+    }
+}
+
+pub fn scan_folder(folder: &str, parallel: bool) -> Result<ScanResult, ScanError> {
     let result: DashMap<String, EnvMetadata> = DashMap::new();
     let processed_files = AtomicUsize::new(0);
 
-    WalkDir::new(folder)
-        .into_iter()
-        .par_bridge()
-        .filter_map(|entry| entry.ok())
-        .filter(|entry| {
-            #[allow(unnecessary_map_or)]
-            entry.path().extension().map_or(false, |extension| {
-                let ext_str = extension.to_str();
-                if let Some(ext_str) = ext_str {
-                    FILE_EXTENSION.contains(&ext_str)
-                } else {
-                    false
-                }
-            })
-        })
-        .for_each(|entry| {
-            let content = std::fs::read_to_string(entry.path());
-            if let Err(err) = content {
-                eprintln!("Error reading file: {}", err);
-                return;
-            }
-
-            processed_files.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
-            let content = content.unwrap();
-            for cap in ENV_VAR_TS_DEFAULT_PATTERN.captures_iter(&content) {
-                let var_name = String::from(&cap[1]);
-                let current_var_ammount = result.get_mut(&var_name);
-                let place = entry.path().to_string_lossy().into_owned();
-
-                match current_var_ammount {
-                    Some(mut value) => {
-                        value.register_place(place);
-                    }
-                    None => {
-                        let mut new_env = EnvMetadata::new(var_name.clone());
-                        new_env.register_place(place);
-                        result.insert(var_name, new_env);
-                    }
-                }
-            }
-        });
+    if parallel {
+        WalkDir::new(folder)
+            .into_iter()
+            .par_bridge()
+            .filter_map(|entry| entry.ok())
+            .filter(filter_dir)
+            .for_each(|entry| for_each_file(entry, &result, &processed_files));
+    } else {
+        WalkDir::new(folder)
+            .into_iter()
+            .filter_map(|entry| entry.ok())
+            .filter(filter_dir)
+            .for_each(|entry| for_each_file(entry, &result, &processed_files));
+    }
 
     let final_result = result
         .iter()
