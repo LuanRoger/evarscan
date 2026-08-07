16979edeb3c8b8e420ca36b6e1f61878451bfaf7
ROG <luanroger.dev@gmail.com>
2026-08-07 16:46:13 -0300

    perf: use memory buffers and parallel line reads

--- src/sniffer.rs
+++ src/sniffer.rs
@@ -1,4 +1,8 @@
-use std::sync::atomic::AtomicUsize;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    sync::{Arc, atomic::AtomicUsize},
+};
 
 use dashmap::DashMap;
 use rayon::prelude::*;
@@ -11,19 +15,19 @@ pub struct ScanError(pub String);
 
 #[derive(Debug, Serialize, Clone)]
 pub struct EnvMetadata {
-    key: String,
+    key: Arc<str>,
     appearances: usize,
-    places: Vec<String>,
+    places: Vec<Arc<str>>,
 }
 
 #[derive(Debug, Serialize)]
 pub struct ScanResult {
-    envs: Vec<EnvMetadata>,
+    envs: Vec<Arc<EnvMetadata>>,
     processed_files: usize,
 }
 
 impl EnvMetadata {
-    pub fn new(key: String) -> Self {
+    pub fn new(key: Arc<str>) -> Self {
         Self {
             key,
             appearances: 0,
@@ -31,14 +35,14 @@ impl EnvMetadata {
         }
     }
 
-    pub fn register_place(&mut self, place: String) {
+    pub fn register_place(&mut self, place: Arc<str>) {
         self.places.push(place);
         self.appearances += 1;
     }
 }
 
 impl ScanResult {
-    fn new(envs: Vec<EnvMetadata>, processed_files: usize) -> Self {
+    fn new(envs: Vec<Arc<EnvMetadata>>, processed_files: usize) -> Self {
         Self {
             envs,
             processed_files,
@@ -47,10 +51,6 @@ impl ScanResult {
 
     pub fn processed_files(&self) -> usize {
         self.processed_files
-    }
-
-    pub fn envs(&self) -> &[EnvMetadata] {
-        &self.envs
     }
 }
 
@@ -65,39 +65,69 @@ fn filter_dir(entry: &walkdir::DirEntry) -> bool {
     })
 }
 
+fn process_line(
+    entry: &walkdir::DirEntry,
+    line: &str,
+    result: &DashMap<Arc<str>, Arc<EnvMetadata>>,
+) {
+    ENV_VAR_TS_DEFAULT_PATTERN
+        .captures_iter(line)
+        .for_each(|cap| {
+            let var_name = &cap[1];
+            let current_var_ammount = result.get_mut(var_name);
+            let place = entry.path().to_str();
+
+            match current_var_ammount {
+                Some(mut value) => {
+                    if let Some(place) = place {
+                        Arc::make_mut(&mut value).register_place(Arc::from(place));
+                    }
+                }
+                None => {
+                    if let Some(place) = place {
+                        let new_var: Arc<str> = Arc::from(var_name);
+                        let mut new_env = EnvMetadata::new(new_var.clone());
+                        new_env.register_place(Arc::from(place));
+                        result.insert(new_var.clone(), Arc::from(new_env));
+                    }
+                }
+            }
+        })
+}
+
 fn for_each_file(
     entry: walkdir::DirEntry,
-    result: &DashMap<String, EnvMetadata>,
+    result: &DashMap<Arc<str>, Arc<EnvMetadata>>,
     processed_files: &AtomicUsize,
+    parallel: bool,
 ) {
-    let content = std::fs::read_to_string(entry.path());
-    if let Err(err) = content {
-        eprintln!("Error reading file: {}", err);
-        return;
-    }
+    let file = File::open(entry.path());
+    let reader = match file {
+        Ok(file) => BufReader::new(file),
+        Err(err) => {
+            eprintln!("Error opening file: {}", err);
+            return;
+        }
+    };
 
     processed_files.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
-    let content = content.unwrap();
-    for cap in ENV_VAR_TS_DEFAULT_PATTERN.captures_iter(&content) {
-        let var_name = String::from(&cap[1]);
-        let current_var_ammount = result.get_mut(&var_name);
-        let place = entry.path().to_string_lossy().into_owned();
-
-        match current_var_ammount {
-            Some(mut value) => {
-                value.register_place(place);
+    if parallel {
+        reader.lines().par_bridge().for_each(|line| {
+            if let Ok(line) = line {
+                process_line(&entry, &line, result);
             }
-            None => {
-                let mut new_env = EnvMetadata::new(var_name.clone());
-                new_env.register_place(place);
-                result.insert(var_name, new_env);
+        });
+    } else {
+        reader.lines().for_each(|line| {
+            if let Ok(line) = line {
+                process_line(&entry, &line, result);
             }
-        }
+        });
     }
 }
 
 pub fn scan_folder(folder: &str, parallel: bool) -> Result<ScanResult, ScanError> {
-    let result: DashMap<String, EnvMetadata> = DashMap::new();
+    let result: DashMap<Arc<str>, Arc<EnvMetadata>> = DashMap::new();
     let processed_files = AtomicUsize::new(0);
 
     if parallel {
@@ -106,20 +136,21 @@ pub fn scan_folder(folder: &str, parallel: bool) -> Result<ScanResult, ScanError
             .par_bridge()
             .filter_map(|entry| entry.ok())
             .filter(filter_dir)
-            .for_each(|entry| for_each_file(entry, &result, &processed_files));
+            .for_each(|entry| for_each_file(entry, &result, &processed_files, true));
     } else {
         WalkDir::new(folder)
             .into_iter()
             .filter_map(|entry| entry.ok())
             .filter(filter_dir)
-            .for_each(|entry| for_each_file(entry, &result, &processed_files));
+            .for_each(|entry| for_each_file(entry, &result, &processed_files, false));
     }
 
     let final_result = result
         .iter()
-        .map(|entry| (*entry.value()).clone())
+        .map(|entry| entry.value().clone())
         .collect::<Vec<_>>();
     let processed_files = processed_files.load(std::sync::atomic::Ordering::Relaxed);
+    drop(result);
 
     Ok(ScanResult::new(final_result, processed_files))
 }
